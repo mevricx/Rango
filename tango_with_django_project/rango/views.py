036730dc51ae07e18131c4d2b04968cb5b6e6a38
mevricx <mevricx@gmail.com>
2015-09-15 09:46:56 +0000

    Created about page for rango

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello world!")
+	return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
+
+def about(request):
+	return HttpResponse("Things and such. Back to <a href="/rango/">Index</a>")
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-	url(r'^', include(app_urls)), # Added URL
+	url(r'^rango/', include('rango.urls')), # Added URL
+	url(r'^rango/about/', include('rango.urls')), # About Page
 )
